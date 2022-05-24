e6d1d6e719d8106ee436b00261cf1240da0f6d8c
Saadhik shaik <96807695+SADHIKSK@users.noreply.github.com>
2022-05-24 22:02:55 +0530

    #include<stdio.h> int main() {     int first=0,second=1,next=0,num;     scanf("%d",&num);     while(num>next)     {         next=first+second;         first=second;         second=next;     }     if(num==next)     {         printf("True");     }     else     {         printf("False");     } }

--- Fibonacci_or_not.c
+++ Fibonacci_or_not.c
@@ -1,32 +1,20 @@
-
-  #include <stdio.h>
-  int main() {
-        int data, num1 = 0, num2 = 1, temp, flag = 0;
-
-    
-        scanf("%d", &data);
-
-            if (data == num1 || data == num2) {
-                printf("%d is a fibonacci number
-", data);
-                return 0;
-        }
-
-      
-        while (num2 <= data) {
-                temp = num2;
-                num2 = num1 + num2;
-                num1 = temp;
-                if (num2 == data) {
-                        flag = 1;
-                        break;
-                }
-        }
-
-        if (flag) {
-                printf("True");
-        } else {
-                printf("False");
-        }
-        return 0;
-  }
+#include<stdio.h>
+int main()
+{
+    int first=0,second=1,next=0,num;
+    scanf("%d",&num);
+    while(num>next)
+    {
+        next=first+second;
+        first=second;
+        second=next;
+    }
+    if(num==next)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
