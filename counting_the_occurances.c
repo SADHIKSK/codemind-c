fdbecc421b6388c1428721a9f4ef3034b4524b2e
Saadhik shaik <96807695+SADHIKSK@users.noreply.github.com>
2022-06-29 23:59:39 +0530

    #include<stdio.h> int main() {     int count=0;     char str[1000],ch;     scanf("%[^ ]s",str);     scanf(" %c",&ch);     for(int i=0;str[i]!=NULL;i++)     {         if(str[i]==ch)         count++;     }     if(count!=0)     printf("%d",count);     else     printf("-1"); }

---
+++ counting_the_occurances.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int count=0;
+    char str[1000],ch;
+    scanf("%[^
+]s",str);
+    scanf("
+%c",&ch);
+    for(int i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]==ch)
+        count++;
+    }
+    if(count!=0)
+    printf("%d",count);
+    else
+    printf("-1");
+}
